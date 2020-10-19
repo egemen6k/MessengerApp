7a06fa67db349f8db252866012f8d12a9436e095
egemen6k <egemen.altikardesler@gmail.com>
2020-10-19 19:55:48 +0300

    +FireBaseAuth accessed +DatabaseRef accessed +Register page edittext accessed for username,email,pass +Register page conditional logic added

--- app/src/main/java/com/example/messengerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/messengerapp/RegisterActivity.kt
@@ -3,9 +3,17 @@ package com.example.messengerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var refUsers : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -18,6 +26,28 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this,WelcomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        mAuth  = FirebaseAuth.getInstance()
+
+        register_btn.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser() {
+        val username:  String  =  username_register.text.toString()
+        val email:  String  =  email_register.text.toString()
+        val password:  String  =  password_register.text.toString()
+
+        if(username  ==  ""){
+            Toast.makeText(this,"Please write username.",Toast.LENGTH_LONG).show()
+        }else if(email == ""){
+            Toast.makeText(this,"Please write email.",Toast.LENGTH_LONG).show()
+        }else if(password == ""){
+            Toast.makeText(this,"Please write password.",Toast.LENGTH_LONG).show()
+        }else{
+
         }
     }
 }
