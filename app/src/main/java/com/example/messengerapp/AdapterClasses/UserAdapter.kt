6a4b7f608225d865451c50d556e898a0be508f3b
egemen6k <egemen.altikardesler@gmail.com>
2020-10-21 20:41:43 +0300

    +recycler view adapter added +managing user list and recyclerview

---
+++ app/src/main/java/com/example/messengerapp/AdapterClasses/UserAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.messengerapp.AdapterClasses
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.messengerapp.ModelClasses.Users
+import com.example.messengerapp.R
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class UserAdapter (
+    private val mContext : Context,
+    private val mUsers: List<Users>,
+    private val isChatChecked: Boolean
+    ) : RecyclerView.Adapter<UserAdapter.ViewHolder?>()
+{
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user: Users = mUsers[position]
+        holder.usernameTxt.text = user!!.getUserName()
+        Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(mContext).inflate(R.layout.user_search_item_layout,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mUsers.size
+    }
+
+    inner class ViewHolder(itemView: View)  : RecyclerView.ViewHolder(itemView){
+        var usernameTxt: TextView = itemView.findViewById(R.id.user_name)
+        var profileImageView: CircleImageView = itemView.findViewById(R.id.profile_image)
+        var onlineTxt: CircleImageView = itemView.findViewById(R.id.image_online)
+        var oflineTxt: CircleImageView = itemView.findViewById(R.id.image_offline)
+        var lastMessageTxt: TextView = itemView.findViewById(R.id.message_last)
+    }
+
+
+}
--- app/src/main/java/com/example/messengerapp/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/messengerapp/Fragments/SearchFragment.kt
@@ -5,28 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.messengerapp.AdapterClasses.UserAdapter
+import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
+    private var userAdapter: UserAdapter? = null
+    private var mUsers: List<Users>? = null
+
 
     /*factory bileşenleri çıkartıldı*/
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_search, container, false)
+
+        mUsers = ArrayList()
+        retrieveAllUser()
+
+
+       return view
+    }
+
+    private fun retrieveAllUser() {
+        val firebaseUserID = FirebaseAuth.getInstance().currentUser!!.uid
+        /*tüm userları  aldığımız için child(userid) yi sildik*/
+        val refUsers = FirebaseDatabase.getInstance().reference.child("Users")
+
+        refUsers.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                (mUsers as ArrayList<Users>).clear()
+                for (snapshot in p0.children) {
+                    val user: Users? = p0.getValue(Users::class.java)
+                    /*ben hariç bütün userları arrayliste ekle*/
+                    if (!(user!!.getUID()).equals(firebaseUserID)) {
+                        (mUsers as ArrayList<Users>).add(user)
+                    }
+                }
+                    userAdapter = UserAdapter(context!!, mUsers!!, false)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
     }
 
 }
