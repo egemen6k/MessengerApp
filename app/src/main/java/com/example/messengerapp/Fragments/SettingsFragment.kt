1d6d485c966ff59200c66e85d1afde791ab3d478
egemen6k <egemen.altikardesler@gmail.com>
2020-10-26 20:45:38 +0300

    +Managing uploading task

--- app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/messengerapp/Fragments/SettingsFragment.kt
@@ -1,15 +1,26 @@
 package com.example.messengerapp.Fragments
 
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
@@ -18,6 +29,9 @@ class SettingsFragment : Fragment() {
     /*factory bileşenleri kaldırıldı*/
     var usersReference: DatabaseReference? = null
     var firebaseUser: FirebaseUser? = null
+    private val RequestCode = 438
+    private var imageUri: Uri? = null
+    private var storageRef: StorageReference? = null
 
 
     override fun onCreateView(
@@ -29,6 +43,7 @@ class SettingsFragment : Fragment() {
 
         firebaseUser = FirebaseAuth.getInstance().currentUser
         usersReference= FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+        storageRef = FirebaseStorage.getInstance().reference.child("User Images")
 
         usersReference!!.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(p0: DataSnapshot) {
@@ -48,7 +63,65 @@ class SettingsFragment : Fragment() {
             }
         })
 
+        view.profile_image_settings.setOnClickListener {
+            pickImage()
+        }
+
+        view.cover_image_settings.setOnClickListener {
+            pickImage()
+        }
+
         return view
+    }
+
+    private fun pickImage() {
+        val intent = Intent()
+        /*send user to the mobile phone gallery*/
+        intent.type = "image/*"  /*type of the file*/
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, RequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RequestCode && resultCode == Activity.RESULT_OK && data!!.data != null){
+            imageUri = data.data
+            Toast.makeText(context,"uploading...",Toast.LENGTH_LONG).show()
+            uploadImageToDatabase()
+        }
+    }
+
+    private fun uploadImageToDatabase() {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("image is uploading, please wait")
+        progressBar.show()
+
+        if(imageUri != null){
+            val fileRef = storageRef!!.child(System.currentTimeMillis().toString() + ".jpg")
+
+            var uploadTask: StorageTask<*>
+            uploadTask = fileRef.putFile(imageUri!!)
+
+            uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot,
+                    Task<Uri>>{task: Task<UploadTask.TaskSnapshot> ->
+                if(!task.isSuccessful){
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation fileRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
+
+
+                }
+            }
+
+        }
+
     }
 
 }
