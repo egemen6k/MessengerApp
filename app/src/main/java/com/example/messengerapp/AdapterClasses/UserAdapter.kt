60cc1d15b625ec03f6eb90abf8f57907715d0286
egemen6k <egemen.altikardesler@gmail.com>
2020-10-28 18:17:55 +0300

    +SendMessage function creating +Currentuser, toWho, messagecontent determined

--- app/src/main/java/com/example/messengerapp/AdapterClasses/UserAdapter.kt
+++ app/src/main/java/com/example/messengerapp/AdapterClasses/UserAdapter.kt
@@ -1,11 +1,16 @@
 package com.example.messengerapp.AdapterClasses
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.messengerapp.MainActivity
+import com.example.messengerapp.MessageChatActivity
 import com.example.messengerapp.ModelClasses.Users
 import com.example.messengerapp.R
 import com.squareup.picasso.Picasso
@@ -25,6 +30,25 @@ class UserAdapter (
         val user: Users = mUsers[position]
         holder.usernameTxt.text = user!!.getUserName()
         Picasso.get().load(user.getProfile()).placeholder(R.drawable.profile).into(holder.profileImageView)
+
+        holder.itemView.setOnClickListener {
+            val options = arrayOf<CharSequence>(
+                "Send Message",
+                "Visit Profile"
+            )
+            val builder: AlertDialog.Builder = AlertDialog.Builder(mContext)
+            builder.setTitle("What do you want?")
+            builder.setItems(options,DialogInterface.OnClickListener { dialog, position ->
+                if(position == 0){
+                    val intent = Intent(mContext, MessageChatActivity::class.java)
+                    intent.putExtra("visit_id", user.getUID())
+                    mContext.startActivity(intent)
+                }
+                if(position == 0){
+
+            }
+            })
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
--- app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
+++ app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
@@ -2,10 +2,36 @@ package com.example.messengerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_message_chat.*
 
 class MessageChatActivity : AppCompatActivity() {
+
+    var userIdVisit: String? = ""
+    var firebaseUser: FirebaseUser? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_chat)
+
+        intent = intent
+        userIdVisit= intent.getStringExtra("visit_id")
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        send_message_btn.setOnClickListener {
+            val message = text_message.text.toString()
+            if(message == ""){
+                Toast.makeText(this,"Please write a message first...", Toast.LENGTH_LONG).show()
+            }else{
+                sendMessageToUser(firebaseUser!!.uid, userIdVisit, message)
+            }
+        }
+    }
+
+    private fun sendMessageToUser(uid: String, userIdVisit: String?, message: String) {
+
     }
 }
