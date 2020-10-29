e2729248d03a66e73695a1d7d8f786feda349661
egemen6k <egemen.altikardesler@gmail.com>
2020-10-29 17:38:17 +0300

    +Receiver name and profile pic retrieved and displayed

--- app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
+++ app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
@@ -3,9 +3,14 @@ package com.example.messengerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.messengerapp.ModelClasses.Users
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_message_chat.*
 
 class MessageChatActivity : AppCompatActivity() {
@@ -21,6 +26,22 @@ class MessageChatActivity : AppCompatActivity() {
         intent = intent
         userIdVisit= intent.getStringExtra("visit_id")
         firebaseUser = FirebaseAuth.getInstance().currentUser
+
+        val reference = FirebaseDatabase.getInstance().reference
+            .child("Users").child(userIdVisit!!)
+        reference.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                val user: Users? = p0.getValue(Users::class.java)
+
+                username_mchat.text = user!!.getUserName()
+                Picasso.get().load(user.getProfile()).into(profile_image_mchat)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+
 
         send_message_btn.setOnClickListener {
             val message = text_message.text.toString()
@@ -51,11 +72,11 @@ class MessageChatActivity : AppCompatActivity() {
                     val chatsListReference = FirebaseDatabase.getInstance().reference
                         .child("ChatList")
 
+                    //implement the push notifications using fcm
                     chatsListReference.child("id").setValue(firebaseUser!!.uid)
                     val reference = FirebaseDatabase.getInstance().reference
                         .child("Users").child(firebaseUser!!.uid)
 
-                    //implement the push notifications using fcm
                 }
             }
     }
