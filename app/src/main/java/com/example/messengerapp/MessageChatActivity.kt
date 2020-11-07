b71b6d2b4cf2e46174341a10f7cba96b54814027
egemen6k <egemen.altikardesler@gmail.com>
2020-11-07 17:19:28 +0300

    +toolbar bug fixed from messagechatact +intent removed

--- app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
+++ app/src/main/java/com/example/messengerapp/MessageChatActivity.kt
@@ -52,9 +52,6 @@ class MessageChatActivity : AppCompatActivity() {
         supportActionBar!!.title = ""
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener {
-            val intent = Intent(this@MessageChatActivity,MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
             finish()
         }
 
